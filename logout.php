e5e7b1d57500cf5240a200f2014583eb1bf2767d
Mendes Kamala <mendesekamala@gmail.com>
2024-09-03 21:55:50 +0300

    second

--- logout.php
+++ logout.php
@@ -6,8 +6,4 @@ header("Location: register.php"); // Redirect to the login page
 exit;
 ?>
 
-<!-- added scripts for login/signup links -->
-<script>
-localStorage.removeItem('loggedIn');
-localStorage.removeItem('username');
-</script>
+
